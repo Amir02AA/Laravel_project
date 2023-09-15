d141727eb5314066edf1399b7200a9b4b9668957
amir aghighi <aghighiamir@yahoo.com>
2023-09-15 11:21:38 +0330

    add: add national rout to myRoutes

--- routes/myRoutes.php
+++ routes/myRoutes.php
@@ -1,8 +1,14 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
 \Illuminate\Support\Facades\Route::get('/myRoute',function (){
     return 'hi';
 });
-\Illuminate\Support\Facades\Route::get('/test2',function (){
-    return path;
-});
+//\Illuminate\Support\Facades\Route::get('/test2',function (){
+//    return path;
+//});
 
+Route::get('/national-code/{code}', function ($code) {
+    return $code;
+})->where('code', '^[0-9]{10}$');
